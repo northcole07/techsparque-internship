25d300ec9eaa9fcb298b7f0772f155e218242ddb
northcole07 <nicolemutai80@gmail.com>
2026-07-11 22:17:55 +0300

    Upload student intro program

---
+++ student_intro2.py
@@ -0,0 +1,9 @@
+name=input("what is your name?")
+school=input("what school do you attend?")
+interests=input("what are your interests?")
+internship_goal=input("what is your internship goal?")
+
+print("Name:", name)
+print("School:", school)
+print("Interests:", interests)
+print("Internship goal:", internship_goal)
